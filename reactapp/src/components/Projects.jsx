e04a0815e6ca2044f5f1145ef4ef9f59a0eb548d
N1077433 <joerkerry@gmail.com>
2025-11-23 22:12:11 +0000

    Add easyCalculator project and update styles
    Added Calculator1.png and a new easyCalculator project entry to Projects.jsx. Updated Projects.css to adjust layout, image sizes, padding, and font sizes for improved display and consistency.

--- reactapp/src/components/Projects.jsx
+++ reactapp/src/components/Projects.jsx
@@ -1,13 +1,13 @@
 import './Projects.css'; 
 import GeoWars from '../assets/GeometryWars.jpg';
-import MyFirstWebsite from '../assets/MyFirstWebsite.png';
 import SoftwareProgrammingImage from '../assets/SoftwareProgrammingImage.jpg';
+import Calculator1 from '../assets/Calculator1.png';
 
 const Projects = () => 
     {
   const projects = [
     {
-      id: 2,
+      id: 1,
       title: "Geometry Wars 2D Remake in Xna Framework to learn a more practical demonstration of C#.",
       category: "full-stack",
       description: "A personalised remake of the classic Geometry Wars game. Learning the basics of C# syntax.",
@@ -16,13 +16,22 @@ const Projects = () =>
       githubUrl: "https://github.com/Joekrry/Geometry-Wars-2D-Remake-in-Xna"
     },
     {
-      id: 4,
+      id: 2,
       title: "Basic mini Projects from my 'Software Programming' Module.",
       category: "front-end",
       description: "Mini projects from my 'Software Programming' module at university. These projects were created using C# and focused on various programming concepts and techniques.",
       technologies: ["C#"],
       image: SoftwareProgrammingImage,
       githubUrl: "https://olympus.ntu.ac.uk/N1077433?tab=repositories"
+    },
+    {
+      id: 3,
+      title: "easyCalculator",
+      category: "back-end",
+      description: "A simple calculator console application built with the purpose of practicing real time console display updating and referencing.",
+      technologies: ["C#"],
+      image: Calculator1,
+      githubUrl: "https://github.com/Joekrry/easyCalculator"
     },
 
   ];
